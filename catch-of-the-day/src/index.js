b17549b7de4186142e26eadf1c3cc5e142f8c521
2hill <jamil.rahou@laposte.net>
2017-10-08 22:21:47 +0200

    jsx and css updated

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,5 +2,5 @@
 import React from 'react';
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
-
+import './css/style.css';
 render (<StorePicker/>, document.querySelector('#main'));
