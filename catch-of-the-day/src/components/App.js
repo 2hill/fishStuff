d62a47ce3b483641037be98d850895f9046400fc
2hill <jamil.rahou@laposte.net>
2017-10-11 01:53:22 +0200

    router part2

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Order from './Order';
+import NotFound from './NotFound';
 import Inventory from './Inventory';
+import StorePicker from './StorePicker';
+import {BrowserRouter, Miss, Match} from 'react-router';
 
 class App extends React.Component {
   render() {
-    return ( 
+    return (
       <div className="catch-of-the-day">
         <div className="menu">
            <Header tagline="Fresh Seafood Market"/>
@@ -17,3 +20,17 @@ class App extends React.Component {
   }
 }
 export default App;
+
+export class Route extends React.Component {
+    render () {
+      return (
+  <BrowserRouter>
+    <div>
+      <Match exactly pattern="/" component={StorePicker} />
+      <Match pattern="/store/:storeId" component={App}/>
+      <Miss component={NotFound}/>
+    </div>
+  </BrowserRouter>
+    )
+  }
+}
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,9 +1,9 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import {BrowserRouter, Miss, Match} from 'react-router';
 import App from './components/App';
-import StorePicker from './components/StorePicker';
+import {Route} from './components/App';
 import './css/style.css';
 
-render (<App/>, document.querySelector('#main'));
+
+render (<Route/>, document.querySelector('#main'));
