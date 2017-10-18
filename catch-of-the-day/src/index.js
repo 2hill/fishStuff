30c391bc930ede78ee83d313a37545eb37d09788
Jamil <jamil.rahou@laposte.net>
2017-10-18 22:35:15 +0200

    Update index.js

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,6 @@
-
 import React from 'react';
 import { render } from 'react-dom';
 import Route from './components/Routing';
 import './css/style.css';
-
 
 render (<Route/>, document.querySelector('#main'));
