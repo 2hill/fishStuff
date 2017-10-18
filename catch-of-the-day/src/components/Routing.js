84de4da6f11a178c24297ac2358014cdb34496c6
Jamil <jamil.rahou@laposte.net>
2017-10-18 22:39:57 +0200

    Update Routing.js

--- catch-of-the-day/src/components/Routing.js
+++ catch-of-the-day/src/components/Routing.js
@@ -4,19 +4,17 @@ import NotFound from './NotFound';
 import StorePicker from './StorePicker';
 import {BrowserRouter, Miss, Match} from 'react-router';
 
-
  class Route extends React.Component {
     render () {
       return (
-  <BrowserRouter>
-    <div>
-      <Match exactly pattern="/" component={StorePicker} />
-      <Match pattern="/store/:storeId" component={App}/>
-      <Miss component={NotFound}/>
-    </div>
-  </BrowserRouter>
+       <BrowserRouter>
+         <div>
+           <Match exactly pattern="/" component={StorePicker} />
+           <Match pattern="/store/:storeId" component={App}/>
+           <Miss component={NotFound}/>
+         </div>
+       </BrowserRouter>
     )
   }
 }
-
 export default Route;
